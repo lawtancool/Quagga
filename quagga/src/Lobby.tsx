30a5122b55ebbc1c9cd09f905d2640b5bd5391e6
lawtancool <26829131+lawtancool@users.noreply.github.com>
2022-10-22 15:41:31 -0700

    Redo in typescript, add react-router-dom

---
+++ quagga/src/Lobby.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {useLoaderData} from "react-router-dom";
+
+function Lobby() {
+  const lobbyId: any = useLoaderData();
+  return (
+      <div>{lobbyId}</div>
+  );
+}
+
+export default Lobby;
---
+++ quagga/src/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Lobby from "./Lobby";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: ":lobbyId",
+        element: <Lobby />,
+        loader: ({ params }) => {
+            return params.lobbyId;
+        },
+    },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
