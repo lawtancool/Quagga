db61534bb3c4a277bb9e38596a1255ea307ec284
lawtancool <26829131+lawtancool@users.noreply.github.com>
2022-10-22 20:10:04 -0700

    Fix Registration

--- quagga/src/Registration.tsx
+++ quagga/src/Registration.tsx
@@ -1,37 +1,22 @@
-import React, {useEffect, useState} from 'react';
-import {useLoaderData} from "react-router-dom";
+import React from 'react';
+import {useParams} from "react-router-dom";
 import {database} from "./firebase";
-import {ref, get, push, set, child} from "firebase/database";
+import {ref, push} from "firebase/database";
 
 
 function Registration() {
-    const lobbyId: any = useLoaderData();
-    const [lobbyData, setLobbyData] = useState();
-
-    useEffect(() => {
-        async function getData() {
-            const data = await get(ref(database, 'games/' + lobbyId));
-            setLobbyData(data.val());
-        }
-
-        if (!lobbyData) {
-            getData();
-        }
-    })
-
-
+    const lobbyId: any = useParams().lobbyId;
 
     async function register (event: React.MouseEvent<HTMLButtonElement>) {
         event.preventDefault();
         const name = document.getElementById("name") as HTMLInputElement | null;
-        const status = set(ref(database, 'games/' + lobbyId + '/name'), name?.value);
+        const status = push(ref(database, 'games/' + lobbyId + '/name'), name?.value);
     }
 
     return (
         <div>
             <input type="text" id="name"/><br/>
             <button onClick={(register)}>Join Game</button><br/>
-            {lobbyData ? lobbyData : "Link doesn't exist"}
         </div>
     );
 }
