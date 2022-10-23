e00ef3ee092026545f9e1dba418e2bc16ce9f7f9
by1459 <by1459@cs.washington.edu>
2022-10-23 01:14:32 -0700

    merge conflicts resolved

--- quagga/src/QuestionEntry.tsx
+++ quagga/src/QuestionEntry.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {useParams} from "react-router-dom";
 import {database} from "./firebase";
-import {ref, push} from "firebase/database";
+import {ref, push, get, set} from "firebase/database";
 import logo from './images/logo.png';
 
 function QuestionEntry() {
@@ -11,7 +11,26 @@ function QuestionEntry() {
     async function register (event: React.MouseEvent<HTMLButtonElement>) {
         event.preventDefault();
         const name = document.getElementById("name") as HTMLInputElement | null;
+        let qs = -1;
+        let players = -1;
         push(ref(database, 'games/' + lobbyId + '/questions'), name?.value).then(() => {setSubmittedQuestion(true)});
+        get(ref(database, 'games/' + lobbyId + '/questions')).then((snapshot) => {
+            if (snapshot.exists()){
+                console.log(snapshot.val())
+                console.log(snapshot.val().size)
+            }
+            qs = snapshot.val().length;
+        })
+        get(ref(database, 'games/' + lobbyId + '/name')).then((snapshot) => {
+            if (snapshot.exists()){
+                console.log(snapshot.val())
+                console.log(snapshot.val().size)
+            }
+            players = snapshot.val().length;
+        })
+        if (players == qs && players != -1 && qs != -1){
+            set(ref(database, 'games/' + lobbyId + '/gameState'), 'answerEntry');
+        }
     }
 
     return (
