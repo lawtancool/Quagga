190f198e41b985e43557e3f6b07eebe524afbd5d
by1459 <by1459@cs.washington.edu>
2022-10-22 22:09:00 -0700

    lobby more

--- quagga/src/Lobby.tsx
+++ quagga/src/Lobby.tsx
@@ -1,30 +1,52 @@
-import React, {Dispatch, SetStateAction, useEffect} from 'react';
+import React, {Dispatch, SetStateAction, useEffect, useRef, useState} from 'react';
 import {useParams, RoutesProps} from "react-router-dom";
 import {database} from "./firebase";
-import {ref, push, get, onValue} from "firebase/database";
+import {ref, push, get, onValue, child} from "firebase/database";
 
 
 function Lobby() {
     const lobbyId: any = useParams().lobbyId;
-    let names: string[] = [];
+    const [names, setNames] = useState([""]);
+    const lobbyRef = ref(database, 'games/' + lobbyId);
+    const namesRef = child(lobbyRef, 'name');
+
+    let temp: string[] = [];
 
     useEffect(() => {
-        async function getNames () {
-            get(ref(database, 'games/' + lobbyId + '/name')).then((snapshot) => {
-                snapshot.forEach((snapshot) => {
-                    console.log(snapshot.val())
-                    names.push(snapshot.val())
-                })
-
+        // async function getNames () {
+        //     const temp: string[] = [];
+        //     get(ref(database, 'games/' + lobbyId + '/name')).then((snapshot) => {
+        //         snapshot.forEach((snapshot) => {
+        //             console.log(snapshot.val())
+        //             temp.push(snapshot.val())
+        //         })
+        //
+        //     })
+        //     setNames(temp);
+        // }
+        // if (names.length < ) {
+        //     getNames();
+        // }
+        onValue(namesRef, (snapshot) => {
+            temp = [];
+            snapshot.forEach((snapshot) => {
+                console.log(snapshot.val())
+                temp.push(snapshot.val())
             })
-        }
-        getNames();
+            setNames(temp);
+        })
     })
+    console.log(names);
     return (
         <div>
             <p>Names:</p><br/>
             <ul>
-                {names}
+                <li>hi</li>
+                {
+                    names.map((name, index) => (
+                        <li key={index}>{name}</li>
+                    ))
+                }
             </ul>
         </div>
     );
