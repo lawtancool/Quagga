b969dc7475ed61d26e27fe4f2783f0049daba7cc
sazwu <szwu00@gmail.com>
2022-10-22 22:10:47 -0700

    quAGGA

--- quagga/src/FrontPage.tsx
+++ quagga/src/FrontPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react';
 import {database} from "./firebase";
 import {push, ref} from "firebase/database";
 import { useNavigate } from "react-router-dom";
@@ -6,11 +7,17 @@ import logo from './logo.png';
 
 function FrontPage() {
     const newLinkKey = push(ref(database, 'games'), {gameState: "lobby"}).key;
+    const [link, setLink] = useState({link: `quagga.io/${newLinkKey}`});
 
     let navigate = useNavigate(); 
-    const routeChange = () =>{ 
+    const routeChange = () => { 
         let path = `${newLinkKey}`; 
         navigate(path);
+    }
+    
+    const copyTooltip = () => {
+        navigator.clipboard.writeText(`quagga.io/${newLinkKey}`)
+        setLink({...link, link: `Copied!`});
     }
 
     return (
@@ -18,8 +25,8 @@ function FrontPage() {
             <img src={logo} className='logo'/>
             <div className='h1'>QUAGGA</div>
             <div>
-                <input className='input-box' value={`quagga.lawrencetan.ca/${newLinkKey}`}></input>
-                <button className='link-button'> COPY LINK </button>
+                <input className='input-box' value={link.link}></input>
+                <button className='link-button' onClick={copyTooltip}> COPY LINK </button>
             </div>
             <button className='long-button' onClick={routeChange}> NEXT </button>
         
