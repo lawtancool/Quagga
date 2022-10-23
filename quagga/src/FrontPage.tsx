90465834f168bce9b939acb3c3f332269af82cc1
sazwu <szwu00@gmail.com>
2022-10-22 20:49:27 -0700

    Yuhhhhhhh

--- quagga/src/FrontPage.tsx
+++ quagga/src/FrontPage.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {database} from "./firebase";
 import {push, ref} from "firebase/database";
+import logo from './logo.png';
 
 function FrontPage() {
     const newLinkKey = push(ref(database, 'games'), {gameState: "lobby"}).key;
 
     return (
         <div>
-            {newLinkKey}
+            <img src={logo} className='logo'/>
+            <div className='h1'>QUAGGA</div>
+            <div className='link'>
+                <input value={`quagga.lawrencetan.ca/${newLinkKey}`}></input>
+                <button> COPY LINK </button>
+            </div>
+            <button className='long-button'> NEXT </button>
         </div>
     );
 }
