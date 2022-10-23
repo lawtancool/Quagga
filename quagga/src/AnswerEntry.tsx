51eb98b09b2bc3c28b33f8a68f3d01ac66e47e84
by1459 <by1459@cs.washington.edu>
2022-10-23 02:42:19 -0700

    Added answer entry page with questions

--- quagga/src/AnswerEntry.tsx
+++ quagga/src/AnswerEntry.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import {database} from "./firebase";
 import {ref, push, get, set} from "firebase/database";
@@ -7,28 +7,27 @@ import {ref, push, get, set} from "firebase/database";
 function AnswerEntry() {
     const lobbyId: any = useParams().lobbyId;
     const [submittedAnswer, setSubmittedAnswer] = useState(false);
+    const [question, setQuestion] = useState("");
+    const lastQ: number = 0;
 
-    async function register (event: React.MouseEvent<HTMLButtonElement>) {
-        event.preventDefault();
-        const name = document.getElementById("name") as HTMLInputElement | null;
-        let qs = -1;
-        let players = -1;
-        push(ref(database, 'games/' + lobbyId + '/questions'), name?.value).then(() => {setSubmittedAnswer(true)});
-        get(ref(database, 'games/' + lobbyId + '/questions')).then((snapshot) => {
-            if (snapshot.exists()){
-                console.log(snapshot.val().length)
-            }
-            qs = snapshot.val().length;
-        })
-        get(ref(database, 'games/' + lobbyId + '/name')).then((snapshot) => {
-            if (snapshot.exists()){
-                console.log(snapshot.val().length)
-            }
-            players = snapshot.val().length;
-        })
-        if (players == qs){
-            set(ref(database, 'games/' + lobbyId + '/gameState'), 'answerEntry');
+    useEffect(() => {
+        async function getQuestion() {
+            let count = 0;
+            get(ref(database, 'games/' + lobbyId + '/questions')).then((snapshot) => {
+                snapshot.forEach((snapshot) => {
+                    if (count == lastQ){
+                        setQuestion(snapshot.val());
+                        console.log(snapshot.val());
+                    }
+                    count++;
+                })
+            })
         }
+        getQuestion();
+    })
+
+    async function answer (event: React.MouseEvent<HTMLButtonElement>) {
+
     }
 
     return (
@@ -37,8 +36,9 @@ function AnswerEntry() {
                 :
                 <div>
                     <h1>Write your response!</h1>
+                    <p>{question}</p>
                     <input type="text" id="name"/><br/>
-                    <button onClick={(register)}>Submit answer</button><br/>
+                    <button onClick={(answer)}>Submit answer</button><br/>
                 </div>
         }
         </>
