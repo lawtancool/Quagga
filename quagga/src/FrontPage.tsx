15fbcadd9901a074c53d36bd40d52bac318b002b
lawtancool <26829131+lawtancool@users.noreply.github.com>
2022-10-22 17:47:56 -0700

    Working read from database

--- quagga/src/FrontPage.tsx
+++ quagga/src/FrontPage.tsx
@@ -3,7 +3,7 @@ import {database} from "./firebase";
 import {push, ref} from "firebase/database";
 
 function FrontPage() {
-    const newLinkKey = push(ref(database, 'games'), '').key;
+    const newLinkKey = push(ref(database, 'games'), 'coconut').key;
 
     return (
         <div>
--- quagga/src/Lobby.tsx
+++ quagga/src/Lobby.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useLoaderData} from "react-router-dom";
 import {database} from "./firebase";
-import {ref, get, DataSnapshot} from "firebase/database";
+import {ref, get} from "firebase/database";
 
 function Lobby() {
     const lobbyId: any = useLoaderData();
-    const lobbyData: DataSnapshot = await get(ref(database, lobbyId));
+    const [lobbyData, setLobbyData] = useState();
+
+    useEffect(() => {
+        async function getData() {
+            console.log(lobbyId);
+            const data = await get(ref(database, 'games/' + lobbyId));
+            console.log(data.exists());
+            setLobbyData(data.val());
+        }
+
+        if (!lobbyData) {
+            getData();
+        }
+    })
     return (
-        <div>{lobbyData.val()}</div>
+        <div>{lobbyData}</div>
     );
 }
 
